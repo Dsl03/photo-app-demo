c97b7a99213f14bdb1fc43e00c1543a9f0e0024b
Dsl03 <54111496+Dsl03@users.noreply.github.com>
2022-05-19 22:32:25 -0500

    most recent work

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -7,6 +7,143 @@ const story2Html = story => {
     `;
 };
 
+const handleBookmark = ev => {
+    console.log("Handle bookmark functionality")
+};
+
+const renderLikeButton = post => {
+    if (post.current_user_like_id) {
+        return `
+        <button 
+            data-post-id = "${post.id}"
+            data-like-id = "${post.current_user_like_id}"
+            aria-label = "Like/ Unlike"
+            aria-checked = "true"
+            onclick ="handleLike(event);">
+            <i class = "fas fa-heart"></i>
+        </button>
+        `;
+    } else {
+        return  `
+        <button 
+            data-like-id = "${post.current_user_like_id}"
+            aria-label = "Like/ Unlike"
+            aria-checked = "false"
+            onclick ="handleLike(event);">
+            <i class = "far fa-heart"></i>
+        </button>
+        `;
+    }
+}; 
+
+const renderBookmarkButton = post => {
+    if (post.current_user_bookmark_id) {
+        return `
+        <button 
+            data-post-id = "${post.id}"
+            data-bookmark-id = "${post.current_user_bookmark_id}"
+            aria-label = "Bookmark/ Unbookmark"
+            aria-checked = "true"
+            onclick ="handleBookmark(event);">
+            <i class = "fas fa-bookmark"></i>
+        </button>
+        `;
+    } else {
+        return  `
+        <button 
+            data-like-id = "${post.current_user_bookmark_id}"
+            aria-label = "Bookmark/ Unbookmark"
+            aria-checked = "false"
+            onclick ="handleBookmark(event);">
+            <i class = "far fa-bookmark"></i>
+        </button>
+        `;
+    }
+}; 
+
+const handleLike = ev => {
+    const elem = ev.currentTarget;
+    console.log("Handle like functionality")
+
+    if (elem.getAttribute('aria-checked') === 'true') {
+        unlikePost(elem);
+    } else {
+        console.log('like post');
+        likePost(elem);
+
+    }
+    
+};
+
+
+const unlikePost = elem => {
+    console.log('unlike post');
+    fetch(`/api/posts/likes/${elem.dataset.likeId}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log('redraw the post');
+    });
+    
+
+};
+
+const likePost = elem => {
+    console.log('like post');
+
+    const postData = {
+        "post_id": Number(elem.dataset.postId)
+    }
+
+
+    fetch("/api/posts/likes/", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+
+};
+
+
+
+
+const post2Html = post => {
+    return `
+    <section class = "card">
+        <img src="${post.image_url }"  />
+        <div class = "button-container">
+            ${renderLikeButton(post)}
+            ${renderBookmarkButton(post)}
+
+        </div>
+        <p>${post.caption }</p>
+    </section>
+`;
+
+};
+
+const displayPosts = () => {
+    fetch('/api/posts')
+        .then(response => response.json())
+        .then(posts => {
+            console.log(posts);
+            const html = posts.map(post2Html).join('\n');
+            document.querySelector('#posts').innerHTML = html;
+        })
+};
+
+
 // fetch data from your API endpoint:
 const displayStories = () => {
     fetch('/api/stories')
@@ -117,6 +254,7 @@ const getSuggestions = () => {
 }   
 
 const initPage = () => {
+    displayPosts();
     displayProfile();
     displayStories();
     getSuggestions();
