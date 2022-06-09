c46518407308a36d0d5de939333ab4ca1f82af89
davidyim03 <66441490+davidyim03@users.noreply.github.com>
2022-06-08 22:29:07 -0500

    mostly working

--- src/post.js
+++ src/post.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {getHeaders} from './utils'
 import LikeButton from './LikeButton';
 import BookmarkButton from './BookmarkButton';
+import AddComment from './AddComment';
 
 class Post extends React.Component {
 
@@ -32,13 +33,17 @@ class Post extends React.Component {
 
     render () {
         const post = this.state.post;
+        const boldusername = <strong>bolded</strong>
+
         return (
             <section
                 className="card">
                 <img src={post.image_url}/>
                 <LikeButton refreshPost = {this.refreshPostDataFromServer} likeId={post.current_user_like_id} postId = {post.id}/>
                 <BookmarkButton refreshPost = {this.refreshPostDataFromServer} bookmarkId={post.current_user_bookmark_id} postId = {post.id}/>
-                <p>{post.caption}</p>
+                <p><strong>{post.user.username}</strong>{post.caption}</p>
+                <p><strong>{post.comments[0] ? post.comments[post.comments.length-1].user.username : ""}</strong>{post.comments[0] ? post.comments[post.comments.length-1].text: ""}</p>
+                <AddComment refreshPost = {this.refreshPostDataFromServer} postId = {post.id}/>
             </section>
         )
     }
